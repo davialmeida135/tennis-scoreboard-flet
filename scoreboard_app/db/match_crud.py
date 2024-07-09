8a7d4cbca4f527bb2366f5e86ef77c26cfdadda3
davialmeida135 <davi.almeida.135@ufrn.edu.br>
2024-07-08 23:58:41 -0300

    Update match_crud.py

--- scoreboard_app/db/match_crud.py
+++ scoreboard_app/db/match_crud.py
@@ -1,3 +1,4 @@
+import requests
 def get_data(conn, conditions=None):
     cursor = conn.cursor()
     if conditions:
@@ -11,28 +12,33 @@ def get_data(conn, conditions=None):
     result = [{columns[i]:row[i] for i in range(len(columns))} for row in rows]
     return result
 
-def check_data_exists(conn,condition):
-    cursor = conn.cursor()
-    cursor.execute(f"SELECT EXISTS (SELECT 1 FROM match WHERE {condition})")
-    return cursor.fetchone()[0]==1
+def create_match(api_url, match_data, token):
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json'
+    }
+    response = requests.post(f"{api_url}/matches/new", headers=headers, json=match_data)
+    if response.status_code == 201:
+        return response.json().get('message')
+    else:
+        response.raise_for_status()
+        
+def update_match(api_url, match_id, match_info, token):
+    headers = {'Authorization': f'Bearer {token}', 'Content-Type': 'application/json'}
+    data = {
+        'idMatch': match_id,
+        'match_info': match_info
+    }
+    response = requests.post(f"{api_url}/matches/update", headers=headers, json=data)
+    if response.status_code == 200:
+        return response.json().get('message')
+    else:
+        response.raise_for_status()
 
-def create_match(conn, owner_id, match_info):
-    cursor = conn.cursor()
-    cursor.execute(
-        "INSERT INTO match (owner_id, match_info) VALUES (?, ?)", (owner_id, match_info)
-    )
-    conn.commit()
-
-def update_match(conn, match_id, match_info):
-    cursor = conn.cursor()
-    cursor.execute(
-        "UPDATE match SET match_info = ? WHERE id = ?", (match_info, match_id)
-    )
-    conn.commit()
-
-def delete_match(conn, match_id):
-    cursor = conn.cursor()
-    cursor.execute(
-        "DELETE FROM match WHERE id = ?", (match_id,)
-    )
-    conn.commit()
+def delete_match(api_url, match_id, token):
+    headers = {'Authorization': f'Bearer {token}'}
+    response = requests.delete(f"{api_url}/matches/{match_id}", headers=headers)
+    if response.status_code == 200:
+        return response.json().get('message')
+    else:
+        response.raise_for_status()
